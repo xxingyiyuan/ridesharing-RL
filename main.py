2b864497f1d0821cee6a59497f6f6d6b9b13e5bc
xxingyiyuan <1391065515@qq.com>
2022-09-26 20:48:50 +0800

    1

--- main.py
+++ main.py
@@ -10,13 +10,12 @@ from algorithmCFA import AlgorithmCFA
 from algorithmTSG import AlgorithmTSG
 from algorithmOTMBM import AlgorithmOTMBM
 from algorithmDRL import AlgorithmDRL
-# dataset
-G = Generator(*settings.beijingRange)
+
 # 取消科学计数法
 np.set_printoptions(suppress=True)
 
 
-def initDemands(waitTime, detourRatio, drivers_num, passengers_num, file_num=0, isSave=False):
+def initDemands(generator: Generator, waitTime, detourRatio, drivers_num=40, passengers_num=80):
     """initialize demands
 
     Args:
@@ -29,40 +28,23 @@ def initDemands(waitTime, detourRatio, drivers_num, passengers_num, file_num=0, 
     Returns:
         _type_: drivers_df, passengers_df
     """
-
     st0 = np.random.get_state()
     seed = np.random.set_state(st0)
     random.seed(seed)
     np.random.seed(seed)
+    # -----------------------------------------------
+    # --------------- generate demands --------------
+    # -----------------------------------------------
+    drivers_df = generator.generateRequests(total_num=drivers_num, flag=0)
+    passengers_df = generator.generateRequests(
+        total_num=passengers_num, flag=1)
 
-    # -----------------------------------------------
-    # ---------------- read demands ----------------
-    # -----------------------------------------------
-    if file_num:
-        drivers_df = pd.read_table('./data/driver_requests/driver_requests_{}_{}.txt'.format(drivers_num, file_num), sep=' ',
-                                   header=None, names=['pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'seatNum'])
-        passengers_df = pd.read_table('./data/passenger_requests/passenger_requests_{}_{}.txt'.format(passengers_num, file_num), sep=' ',
-                                      header=None, names=['pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'seatNum'])
-    else:
-        # -----------------------------------------------
-        # --------------- generate demands --------------
-        # -----------------------------------------------
-        drivers_df = G.generateRequests(total_num=drivers_num, flag=0)
-        passengers_df = G.generateRequests(total_num=passengers_num, flag=1)
     # add detour and waitTime
-    drivers_df['detourRatio'] = random.uniform(detourRatio, detourRatio + 0.1)
+    drivers_df['detourRatio'] = random.uniform(
+        detourRatio, detourRatio + 0.1)
     passengers_df['detourRatio'] = random.uniform(
         detourRatio, detourRatio + 0.1)
     passengers_df['waitTime'] = random.uniform(waitTime, waitTime + 1)
-    # -----------------------------------------------
-    # ---------------- store demands ----------------
-    # -----------------------------------------------
-    if isSave:
-        drivers_df.to_csv('./data/driver_requests/driver_requests_{}_{}.txt'.format(
-            drivers_num, file_num), sep=' ', header=None, index=False)
-        passengers_df.to_csv('./data/passenger_requests/passenger_requests_{}_{}.txt'.format(
-            passengers_num, file_num), sep=' ', header=None, index=False)
-
     drivers_demand = [tuple(de) for de in drivers_df.values]
     passengers_demand = [tuple(de) for de in passengers_df.values]
     return drivers_demand, passengers_demand
@@ -77,7 +59,6 @@ def initParticipants(drivers_demand, passengers_demand):
 
 
 def carpool(select, drivers_demand, passengers_demand):
-
     if select == 1:
         drivers, dcaMat, passengers, pcaMat = initParticipants(
             drivers_demand, passengers_demand)
@@ -98,18 +79,100 @@ def carpool(select, drivers_demand, passengers_demand):
         return DRL.collectData()
 
 
-if __name__ == '__main__':
-    total = 1
+def getPartition(leftbottom, regionShape, squreSize):
+    initX, initY = leftbottom
+    leftbottoms = []
+    for i in range(2):
+        y = initY + i*regionShape[1]*squreSize[1]
+        for j in range(2):
+            x = initX + j*regionShape[0]*squreSize[0]
+            leftbottoms.append((x, y))
+    return leftbottoms
+
+
+def generateFiles(datasetName, datasetSettings, fileNums=[0], drivers_num=40, passengers_num=80):
+    # 生成request文件
+    filepath, leftbottom, regionShape, squreSize = datasetSettings
+    leftbottoms = getPartition(leftbottom, regionShape, squreSize)
+    for i in fileNums:
+        leftbottom = random.choice(leftbottoms)
+        generator = Generator(filepath, leftbottom, regionShape, squreSize)
+        drivers_df = generator.generateRequests(total_num=drivers_num, flag=0)
+        passengers_df = generator.generateRequests(
+            total_num=passengers_num, flag=1)
+        drivers_df.to_csv('./data/{}/driver_requests/driver_requests_{}_{}.txt'.format(
+            datasetName, drivers_num, i), sep=' ', header=None, index=False)
+        passengers_df.to_csv('./data/{}/passenger_requests/passenger_requests_{}_{}.txt'
+                             .format(datasetName, passengers_num, i), sep=' ', header=None, index=False)
+
+
+def loadDemand(datasetName, fileNum, waitTime, detourRatio, drivers_num=40, passengers_num=80):
+    drivers_df = pd.read_table('./data/{}/driver_requests/driver_requests_{}_{}.txt'.format(datasetName, drivers_num, fileNum),
+                               sep=' ', header=None, names=['pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'seatNum'])
+    passengers_df = pd.read_table('./data/{}/passenger_requests/passenger_requests_{}_{}.txt'.format(datasetName, passengers_num, fileNum),
+                                  sep=' ', header=None, names=['pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'seatNum'])
+    # add detour and waitTime
+    drivers_df['detourRatio'] = random.uniform(
+        detourRatio, detourRatio + 0.1)
+    passengers_df['detourRatio'] = random.uniform(
+        detourRatio, detourRatio + 0.1)
+    passengers_df['waitTime'] = random.uniform(waitTime, waitTime + 1)
+    drivers_demand = [tuple(de) for de in drivers_df.values]
+    passengers_demand = [tuple(de) for de in passengers_df.values]
+    return drivers_demand, passengers_demand
+
+
+def collectData(datasetName, fileNum, waitTime, detourRatio, drivers_num, passengers_num):
+    drivers_demand, passengers_demand = loadDemand(
+        datasetName, fileNum, waitTime, detourRatio, drivers_num, passengers_num)
     res = np.zeros((4, 5))
-    for i in range(total):
-        for _ in range(6):
-            drivers_demand, passengers_demand = initDemands(
-                waitTime=4, detourRatio=0.5, drivers_num=50, passengers_num=100)
-            res1 = carpool(1, drivers_demand, passengers_demand)
-            res2 = carpool(2, drivers_demand, passengers_demand)
-            res3 = carpool(3, drivers_demand, passengers_demand)
-            res4 = carpool(4, drivers_demand, passengers_demand)
-            tmp = np.array([res1, res2, res3, res4])
-            print(tmp)
-            res += tmp
+    res[0] = carpool(1, drivers_demand, passengers_demand)
+    res[1] = carpool(2, drivers_demand, passengers_demand)
+    res[2] = carpool(3, drivers_demand, passengers_demand)
+    res[3] = carpool(4, drivers_demand, passengers_demand)
     print(res)
+    return res
+
+
+def test(datasetSettings, waitTime, detourRatio):
+    generator = Generator(*datasetSettings)
+    drivers_demand, passengers_demand = initDemands(
+        generator, waitTime, detourRatio)
+    res = np.zeros((4, 5))
+    res[0] = carpool(1, drivers_demand, passengers_demand)
+    res[1] = carpool(2, drivers_demand, passengers_demand)
+    res[2] = carpool(3, drivers_demand, passengers_demand)
+    res[3] = carpool(4, drivers_demand, passengers_demand)
+    print(res)
+
+
+if __name__ == '__main__':
+    dataset = {
+        'beijing': settings.beijingRange,
+        'guangzhou': settings.guangzhouRange,
+        'newyork': settings.newyorkRange
+    }
+    datasetName = 'beijing'
+
+    defaultWaitTime = 4
+    defaultDetourRatio = 0.5
+
+    fileNums = [0]
+    drivers_num = 30
+    passengers_num = 80
+    # generateFiles(datasetName, dataset[datasetName], fileNums, drivers_num, passengers_num)
+    waitTime = defaultWaitTime
+    for detourRatio in [0.2, 0.3, 0.4, 0.5, 0.6]:
+        for file_num in fileNums:
+            tmp = collectData(datasetName, file_num, waitTime,
+                              detourRatio, drivers_num, passengers_num)
+            np.savetxt('./result/{}/detourRatio/file{}-waitTime={}-detourRatio={}.csv'.format(
+                datasetName, file_num, waitTime, detourRatio), tmp, delimiter=',', fmt='%.3f')
+
+    # detourRatio = defaultDetourRatio
+    # for waitTime in range(3, 7):
+    #     for file_num in range(1):
+    #         tmp = collectData(datasetName, file_num, waitTime, detourRatio)
+    #         np.savetxt('./result/{}/file{}-waitTime={}-detourRatio={}.csv'.format(
+    #             datasetName, file_num, waitTime, detourRatio), tmp, delimiter=',', fmt='%.3f')
+    # np.savetxt('./result/{}/waitTime={}-detourRatio={}.csv'.format(datasetName, waitTime, detourRatio), res, delimiter=',', fmt='%.3f')
